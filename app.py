3d8106247abe767ca733bb170adf712ab26151f4
Colin Jacobs <colin@coljac.net>
2021-08-06 13:24:33 +1000

    Added extra parameters

--- app.py
+++ app.py
@@ -8,69 +8,69 @@ import lockdown as ld
 # import matplotlib.pyplot as plt
 
 @st.cache
-def make_figure(results, offset=True):
+def make_figure(results, title=None):
     median = np.median(results)
     
     fig = make_subplots()
 
     fig.add_trace(go.Histogram(x=results, nbinsx=40))
-                            # y=[z['principle'] for z in results],
-                    # hovertemplate = '%{x:.1f} years<br>\nPrinciple: %{y:$,.0f}<extra></extra>',
-                    # mode='lines+markers',
-                    # name='Principle remaining'))
-    # fig.add_trace(go.Scatter(x=[z['month']/12 for z in results], 
-                            # y=[z['interest_paid'] for z in results],
-                    # hovertemplate = '%{x:.1f} years<br>\nInterest: %{y:$,.0f}<extra></extra>',
-                    # mode='lines+markers',
-                    # name='Interest paid'))
-    # if offset:
-        # fig.add_trace(go.Scatter(x=[z['month']/12 for z in results], 
-                            # y=[z['offset'] for z in results],
-                    # mode='lines+markers',
-                    # hovertemplate = '%{x:.1f} years<br>\nOffset: %{y:$,.0f}<extra></extra>',
-                    # name='Offset account balance'))
 
     fig.update_layout(
         width=900, height=600,
                 xaxis_title="Days in lockdown",
-    # yaxis_title="Loan value",
     font = {"size": 10},
-    title=f"Median: {int(median)} days",
-    # legend=dict(
-        # yanchor="top",
-        # y=0.99,
-        # xanchor="left",
-        # x=0.6
+    title=title
     )
 
     return fig
 
 @st.cache
 def get_results(probability_day_outbreak=.01, snap_lockdown_time=3,
-         probability_lockdown_extension=0.5):
-    return ld.sims(N=5000,probability_day_outbreak=probability_day_outbreak, snap_lockdown_time=snap_lockdown_time,
-         probability_lockdown_extension=probability_lockdown_extension)
+         probability_lockdown_extension=0.5,
+         reduce_in_lockdown=False,
+         vaccines=False,
+         vaccine_decay=0
+         ):
+    return ld.sims(N=2000,probability_day_outbreak=probability_day_outbreak, snap_lockdown_time=snap_lockdown_time,
+         probability_lockdown_extension=probability_lockdown_extension,
+         reduce_in_lockdown=reduce_in_lockdown,
+         vaccines=vaccines,
+         vaccine_decay=vaccine_decay
+         )
             
 st.title('Lockdown simulator')
 
 st.sidebar.markdown("## What are the chances of a lockdown?")
-probability_day_outbreak = st.sidebar.slider('Chance of new outbreak per day', value=0.05, min_value=0.01, max_value=.5, step=.01)
+probability_day_outbreak = st.sidebar.slider('Chance of new outbreak per day (%)', value=5, min_value=1, max_value=50, step=1)
 
-snap_lockdown_time = st.sidebar.slider('Snap lockdown initial time', value=7, min_value=3, max_value=21, step=1) 
-probability_lockdown_extension = st.sidebar.slider('Chance lockdown extended', value=.4, min_value=.1, max_value=0.9, step=.1) 
+discount = st.sidebar.checkbox("Reduce outbreak chance during lockdown?", value=True)
 
-st.sidebar.markdown("""Using these values, we perform 5000 simulated years and count the 
+snap_lockdown_time = st.sidebar.slider('Snap lockdown initial time (days)', value=7, min_value=3, max_value=21, step=1) 
+probability_lockdown_extension = st.sidebar.slider('Chance lockdown extended at end (%)', value=40, min_value=5, max_value=90, step=5) 
+
+vaccination = st.sidebar.checkbox("Vaccination reduces outbreak chance?")
+vaccination_rate = st.sidebar.slider("Time to zero outbreak chance (months)", value=12, min_value=3, max_value=36, step=1)
+
+st.sidebar.markdown("""Using these values, we perform 2000 simulated years and count the 
 number of days spent in lockdown for each. The figure to the right is a histogram of number of
 days spent in lockdown.
 
 We assume that when a new breakout occurs during lockdown,
-the lockdown is extended for 7 days; and at the end of each lockdown, if it's extended it's extended
-for 3 days.""")
+the lockdown is extended for 7 days; and when we get to the end of each lockdown, if it's extended (due to continued
+community transmission) it's extended for 3 days. You can modify these values below.""")
 
-results = get_results(probability_day_outbreak=probability_day_outbreak, snap_lockdown_time=snap_lockdown_time, probability_lockdown_extension=probability_lockdown_extension)
+lockdown_outbreak = st.sidebar.slider('Add to lockdown with new outbreak (days)', value=7, min_value=0, max_value=21, step=1) 
+lockdown_failed = st.sidebar.slider('Extend lockdowns by (days)', value=3, min_value=1, max_value=21, step=1) 
 
-fig = make_figure(results)
 
+results = get_results(probability_day_outbreak=probability_day_outbreak/100.0, 
+        snap_lockdown_time=snap_lockdown_time,
+        probability_lockdown_extension=probability_lockdown_extension/100.0,
+        reduce_in_lockdown=discount, 
+        vaccines=vaccination, 
+        vaccine_decay=vaccination_rate)
+
+fig = make_figure(results[0], title=f"Median days locked down: {int(np.median(results[0]))} days, median lockdowns: {int(np.median(results[1]))}")
 st.plotly_chart(fig)
 
 
--- lockdown.py
+++ lockdown.py
@@ -5,37 +5,80 @@ def log(l, s):
         print(str(s))
         
 def trial(l=False, probability_day_outbreak=.01, snap_lockdown_time=3,
-         probability_lockdown_extension=.3, probability_decay=1):
+         probability_lockdown_extension=.3, 
+         reduce_in_lockdown=False,
+         vaccines=False,
+         vaccine_decay=0,
+         add_time=7,
+         extend_time=3
+         ):
     lockdown_days = 0
+    lockdown_count = 0
     lockdown = False
     days_remaining = 0
+
     for i in range(365):
         log(l, f"Day: {i}")
-        days_remaining = max(0, days_remaining-1)
-        if np.random.random() <= probability_day_outbreak:
+        days_remaining = max(0, days_remaining-1) # Days left in current lockdown if any
+        current_chance = probability_day_outbreak # Chance of a new outbreak today
+        if vaccines: # as the population get vaccinated, the outbreak chance will drop
+            # Outbreak chance drops to zero in vaccine_decay months
+            current_chance = probability_day_outbreak + i*(-probability_day_outbreak/(30*vaccine_decay))
+        if reduce_in_lockdown and lockdown: 
+            # If already in lockdown, reduce the outbreak chance by 75%
+            current_chance *= 0.25
+        rolled = np.random.random() # Is there an outbreak today?
+        log(l, f"Chance of outbreak {current_chance:.3f} - rolled {rolled:.3f}")
+        if rolled <= current_chance:
             log(l, "Outbreak!")
             if not lockdown:
+                # Start a new lockdown
                 lockdown = True
+                lockdown_count += 1
                 days_remaining = snap_lockdown_time
             else:
-                days_remaining += 7
+                # New outbreak in existing lockdown, extend the current time a bit
+                days_remaining += add_time
+
         if lockdown and days_remaining <= 0:
-            log(l,"Lockdown ending")
+            # End of a lockdown reached
+            log(l,"Lockdown ending?")
             if np.random.random() < probability_lockdown_extension:
+                # Lockdown extended by a short period
                 log(l,"Nope, short extension")
-                days_remaining += 3
+                days_remaining += extend_time
             else:
+                # End the lockdown for real
                 lockdown = False
-        days_remaining = min(days_remaining, 14)
+
+        # Cap lockdown time to 14 days in the future
+        days_remaining = min(days_remaining, 14) 
+
         if lockdown:
             lockdown_days += 1
         log(l, f"{days_remaining} remaining\n")
-    return lockdown_days
+    return lockdown_days, lockdown_count
 
 def sims(N=1000, probability_day_outbreak=.01, snap_lockdown_time=3,
-         probability_lockdown_extension=.3):
-    results = np.zeros(N)
+         probability_lockdown_extension=.3,
+         reduce_in_lockdown=False,
+         vaccines=False,
+         vaccine_decay=0,
+         add_time=7,
+         extend_time=3
+         ):
+    results = np.zeros(N) # days
+    counts = np.zeros(N) # lockdowns
     for i in range(N):
-        results[i] = trial(probability_day_outbreak=probability_day_outbreak, snap_lockdown_time=snap_lockdown_time,
-         probability_lockdown_extension=probability_lockdown_extension)
-    return results
+        res = trial(probability_day_outbreak=probability_day_outbreak, snap_lockdown_time=snap_lockdown_time,
+         probability_lockdown_extension=probability_lockdown_extension,
+         reduce_in_lockdown=reduce_in_lockdown,
+         vaccines=vaccines,
+         vaccine_decay=vaccine_decay,
+         add_time=add_time,
+         extend_time=extend_time
+         )
+        results[i] = res[0]
+        counts[i] = res[1]
+
+    return results, counts
